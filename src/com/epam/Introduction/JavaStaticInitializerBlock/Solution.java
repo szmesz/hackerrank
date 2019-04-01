a68118dfcf5075f6a0a4c80868e61fc18d557a09
Tamas Vajda <tamas_vajda@epam.com>
2019-04-01 15:59:30 +0200

    JavaStaticInitializerBlock exercise.

---
+++ src/com/epam/Introduction/JavaStaticInitializerBlock/Solution.java
@@ -0,0 +1,41 @@
+package com.epam.Introduction.JavaStaticInitializerBlock;
+
+import java.util.Scanner;
+
+public class Solution {
+
+  static int B;
+  static int H;
+  static boolean flag;
+
+  static void isTrueFlag() throws Exception {
+    if ((0 >= B) || (0 >= H)) {
+      flag = false;
+      throw new Exception();
+    } else {
+      flag = true;
+    }
+  }
+
+  static {
+    Scanner scanner = new Scanner(System.in);
+    H = scanner.nextInt();
+    B = scanner.nextInt();
+
+    try {
+      isTrueFlag();
+    } catch (Exception e) {
+      System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+  }
+
+  public static void main(String[] args) {
+    if (flag) {
+      int area = B * H;
+      System.out.print(area);
+    }
+
+  }
+
+}
+
