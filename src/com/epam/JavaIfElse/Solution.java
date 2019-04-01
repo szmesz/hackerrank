f3dc1dff5ffe33091f451ef0a9b56bf4c0addd66
Tamas Vajda <tamas_vajda@epam.com>
2019-04-01 09:16:19 +0200

    Initial commit

---
+++ src/com/epam/JavaIfElse/Solution.java
@@ -0,0 +1,23 @@
+package com.epam.JavaIfElse;
+
+import java.util.Scanner;
+
+public class Solution {
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int N = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    scanner.close();
+
+    int n = N;
+    String answer = "";
+    if(n % 2 == 1 || (n >= 6 && n <= 20)){
+      answer = "Weird";
+    }else{
+      answer = "Not Weird";
+    }
+    System.out.println(answer);
+  }
+}
---
+++ src/com/epam/JavaLoop1/Solution.java
@@ -0,0 +1,17 @@
+package com.epam.JavaLoop1;
+
+import java.util.Scanner;
+
+public class Solution {
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int N = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    scanner.close();
+    for(int i = 1; i <= 10; i++) {
+      System.out.println(N + " x " + i + " = " +  N * i);
+    }
+  }
+}
