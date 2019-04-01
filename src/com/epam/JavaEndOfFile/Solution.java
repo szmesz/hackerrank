5ffb4e830467715c3761401d1afb1d4e5b8406a1
Tamas Vajda <tamas_vajda@epam.com>
2019-04-01 13:31:21 +0200

    JavaEndOfFile Exercise

---
+++ src/com/epam/JavaEndOfFile/Solution.java
@@ -0,0 +1,15 @@
+package com.epam.JavaEndOfFile;
+
+import java.util.Scanner;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = 1;
+    while (scanner.hasNext()) {
+      System.out.println(n++ + " " + scanner.nextLine());
+    }
+  }
+}
+
