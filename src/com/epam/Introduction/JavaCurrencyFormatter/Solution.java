88ac6f619f80730eabe56a0f912b0732e083e9b6
Tamas Vajda <tamas_vajda@epam.com>
2019-04-02 13:10:26 +0200

    JavaCurrencyFormatter challenge

---
+++ src/com/epam/Introduction/JavaCurrencyFormatter/Solution.java
@@ -0,0 +1,24 @@
+package com.epam.Introduction.JavaCurrencyFormatter;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double payment = scanner.nextDouble();
+    scanner.close();
+
+    NumberFormat nf = NumberFormat.getInstance();
+    Locale INDIA = new Locale("en", "IN");
+
+    System.out.println("US: " + nf.getCurrencyInstance(Locale.US).format(payment));
+    System.out.println("India: " + nf.getCurrencyInstance(INDIA).format(payment));
+    System.out.println("China: " + nf.getCurrencyInstance(Locale.CHINA).format(payment));
+    System.out.println("France: " + nf.getCurrencyInstance(Locale.FRANCE).format(payment));
+  }
+
+}
+
